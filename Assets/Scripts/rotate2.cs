9f7bb5d060120f475f5f3f4e9a40e2746a863a53
rajat4803 <nightfox4803@gmail.com>
2017-11-27 22:36:46 +0530

    animation added in homeScene and rotate script added

--- Assets/Scripts/rotate2.cs
+++ Assets/Scripts/rotate2.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rotate1 : MonoBehaviour {
+public class rotate2 : MonoBehaviour {
 
 	public float speed = 80f;		// rotating speed for circles 
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Vector3.back, speed * Time.deltaTime);  // Time.deltatime so that it doesn't rotate too quickly
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);  // Time.deltatime so that it doesn't rotate too quickly
 	}
 }
