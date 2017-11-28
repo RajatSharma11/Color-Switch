62bfd25b5e17c193d48b6c29bb51b7197de2e082
rajat4803 <nightfox4803@gmail.com>
2017-11-28 11:12:41 +0530

    playScene updated

---
+++ Assets/Scripts/playerjump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerjump : MonoBehaviour {
+
+    public float jumpSpeed = 5f;
+    private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))     // if screen is tapped or left mouse button is pressed; 0 is for left mouse button
+        {
+            body.velocity = Vector2.up * jumpSpeed; // Vector2 is for 2d space
+        }
+	}
+}
