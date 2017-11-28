273ae2d63b1ac83fcb21a1c0b95224663bca3913
rajat4803 <nightfox4803@gmail.com>
2017-11-28 09:39:35 +0530

    functionality added to play button in homeScene

---
+++ Assets/Scripts/playButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playButton : MonoBehaviour {
+
+    // Change Scene when button is pressed
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("playScene");
+    }
+}
